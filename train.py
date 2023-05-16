eeee070c670b0da05a4318c6b4d6215777166184
Le Duc Minh <minhmeo7a1@gmail.com>
2023-05-16 10:30:39 +0700

    Change path to Kaggle

--- train.py
+++ train.py
@@ -19,8 +19,8 @@ from facenet_pytorch import InceptionResnetV1, fixed_image_standardization
 
 
 device = get_device()
-casia_cropped_path = os.path.join(DATA_PATH, 'CASIA-WebFace-cropped/')
-# casia_cropped_path = '/kaggle/input/casia-webface-cropped-with-mtcnn/CASIA-WebFace-cropped'
+# casia_cropped_path = os.path.join(DATA_PATH, 'CASIA-WebFace-cropped/')
+casia_cropped_path = '/kaggle/input/casia-webface-cropped-with-mtcnn/CASIA-WebFace-cropped'
 
 
 def parse_arguments(argv):
